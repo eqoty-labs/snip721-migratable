56ad699b0e047cd4cb16176e798cfe7f5944a114
Luca Spinazzola <mspinluca@gmail.com>
2022-10-05 15:39:20 -0400

    revert debug printing

--- tests/src/jvmTest/kotlin/io/eqoty/dapp/secret/utils/getEnv.kt
+++ tests/src/jvmTest/kotlin/io/eqoty/dapp/secret/utils/getEnv.kt
@@ -1,8 +1,4 @@
 package io.eqoty.dapp.secret.utils
 
-actual fun getEnv(name: String): String? {
-    val a = System.getenv(name)
-    println("getEnv $name:$a")
-    return a
-}
+actual fun getEnv(name: String): String? = System.getenv(name)
 
